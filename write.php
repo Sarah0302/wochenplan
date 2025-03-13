f0990f1db9b2d6467f9b5166a0e54e5c7c6809f2
Sarah Schönthaler <S.Schoenthaler@sdv-werbestudio.de>
2025-03-13 14:38:26 +0100

    datenbank nutzerdaten

--- write.php
+++ write.php
@@ -1,8 +1,14 @@
 <?php
+// Daten aus .env abrufen
+require __DIR__ . '/vendor/autoload.php';
+use Dotenv\Dotenv;
 
-$dsn = "mysql:host=localhost;dbname=wochenplan"; // um auf entsprechende Datenbank zugreifen zu können
-$dbusername = "root";
-$dbpassword = "root";
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$dsn = $_ENV['DB_DSN']; // um auf entsprechende Datenbank zugreifen zu können
+$dbusername = $_ENV['DB_USERNAME'];
+$dbpassword = $_ENV['DB_PASSWORD'];
 
 try {
     $pdo = new PDO($dsn, $dbusername, $dbpassword); // Datenbank verbinden  --> Würde auch außerhalb von try catch funktionieren, dann aber keine Möglichkeit auf Fehler zu reagieren
